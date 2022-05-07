81fe957fd99939874e5fa98b03dfc324c7a69129
Hutomo Sugianto <hutomosugianto@gmail.com>
2022-05-07 12:00:47 +0700

    experience using array

--- src/components/home/Experience.js
+++ src/components/home/Experience.js
@@ -1,4 +1,66 @@
 function Experience() {
+
+  const experiences = [
+    {
+      "id": 1,
+      "title": "IT Manager",
+      "company": "Premiro.com (PT Mitra Ibisnis Terapan)",
+      "period": "April 2020 - December 2021",
+      "descriptionList": [
+        "Managing Product and Technology Team as A Scrum Master and Tech Lead",
+        "Create printed Standard Operating Procedure for SDLC",
+        "Improve communication both in Tech Team and outside of Tech Team",
+        "Improve documentation standards for Technical Team and Product Specifications",
+        "Mentoring for subordinates by regular 1-on-1 and code review"
+      ]
+    },
+    {
+      "id": 2,
+      "title": "Lead Web Developer",
+      "company": "Premiro.com (PT Mitra Ibisnis Terapan)",
+      "period": "October 2017 - March 2021",
+      "descriptionList": [
+        "Migrate System from On Premise to Alibaba Cloud",
+        "Implementation of VT Snap with ReactJS, ExpressJS and GraphQL",
+        "Create Brute Force Handler for User Login Module",
+        "Developing a New Backend Platform using Slim 4 Framework and Stateless Architecture",
+        "Processing Bulk Transaction Using php7 and Redis Queue",
+        "Maintain Legacy System written in Codeigniter Framework"
+      ]
+    },
+    {
+      "id": 3,
+      "title": "Web Developer",
+      "company": "Cekaja.com (PT Puncak Finansial Utama)",
+      "period": "April - September 2017",
+      "descriptionList": [
+        "Manage and Improve Partnership Project with MSIG Online (msigonline.co.id)",
+        "Manage and Improve Partnership Project with Premiro (premiro.com)",
+      ]
+    },
+    {
+      "id": 4,
+      "title": "Web Developer",
+      "company": "KUDO (PT Kudo Teknologi Indonesia)",
+      "period": "December 2015 - April 2017",
+      "descriptionList": [
+        "Prepare the system architecture and tools for the project",
+        "Develop applications using Laravel PHP Framework and Wordpress",
+        "Implement better web and automation technologies (ReactJS, Grunt)",
+      ]
+    },
+    {
+      "id": 5,
+      "title": "Web Developer",
+      "company": "Detik.com (PT Agranet Multicitra Siberkom)",
+      "period": "September 2014 - November 2015",
+      "descriptionList": [
+        "Developer team of SolusiUKM using Panada PHP Framework and PostgreSQL. SolusiUKM is a native ads platform of detik.com.",
+        "Operational and maintenance team of myTrans (VOD and livestreaming platform)",
+      ]
+    },
+  ];
+
   return (
     <>
       {/* experience section */}
@@ -12,73 +74,28 @@ function Experience() {
 
 
           <div className="col-md-9 pt-1">
-            <h5>IT Manager</h5>
 
-            <span className="d-block">Premiro.com (PT Mitra Ibisnis Terapan)</span>
-            <span className="d-block">April 2020 - December 2021</span>
-            <span className="d-block mb-3"></span>
+            {experiences.map((experience) => {
+              return (
+                <>
+                  <h5>{experience.title}</h5>
 
-            <ul className="icon-list">
-              <li>Managing Product and Technology Team as A Scrum Master and Tech Lead</li>
-              <li>Create printed Standard Operating Procedure for SDLC</li>
-              <li>Improve communication both in Tech Team and outside of Tech Team</li>
-              <li>Improve documentation standards for Technical Team and Product Specifications</li>
-              <li>Mentoring for subordinates by regular 1-on-1 and code review</li>
-            </ul>
+                  <span className="d-block">{experience.company}</span>
+                  <span className="d-block">{experience.period}</span>
+                  <span className="d-block mb-3"></span>
 
-            <div className="row mb-3"></div>
-
-            <h5>Lead Web Developer</h5>
-            <span className="d-block">Premiro.com (PT Mitra Ibisnis Terapan)</span>
-            <span className="d-block">October 2017 - March 2021</span>
-            <span className="d-block mb-3"></span>
-
-            <ul className="icon-list">
-              <li>Migrate System from On Premise to Alibaba Cloud</li>
-              <li>Implementation of VT Snap with ReactJS, ExpressJS and GraphQL</li>
-              <li>Create Brute Force Handler for User Login Module</li>
-              <li>Developing a New Backend Platform using Slim 4 Framework and Stateless Architecture</li>
-              <li>Processing Bulk Transaction Using php7 and Redis Queue</li>
-              <li>Maintain Legacy System written in Codeigniter Framework</li>
-            </ul>
-
-            <div className="row mb-3"></div>
-
-            <h5>Web Developer</h5>
-            <span className="d-block">Cekaja.com (PT Puncak Finansial Utama)</span>
-            <span className="d-block">April - September 2017</span>
-            <span className="d-block mb-3"></span>
-
-            <ul className="icon-list">
-              <li>Manage and Improve Partnership Project with MSIG Online (msigonline.co.id)</li>
-              <li>Manage and Improve Partnership Project with Premiro (premiro.com)</li>
-            </ul>
-
-            <div className="row mb-3"></div>
-
-            <h5>Web Developer</h5>
-            <span className="d-block">KUDO (PT Kudo Teknologi Indonesia)</span>
-            <span className="d-block">December 2015 - April 2017</span>
-            <span className="d-block mb-3"></span>
-
-            <ul className="icon-list">
-              <li>Prepare the system architecture and tools for the project</li>
-              <li>Develop applications using Laravel PHP Framework and Wordpress</li>
-              <li>Implement better web and automation technologies (ReactJS, Grunt)</li>
-            </ul>
-
-            <div className="row mb-3"></div>
-
-            <h5>Web Developer</h5>
-            <span className="d-block">Detik.com (PT Agranet Multicitra Siberkom)</span>
-            <span className="d-block">September 2014 - November 2015</span>
-            <span className="d-block mb-3"></span>
-
-            <ul className="icon-list">
-              <li>Developer team of SolusiUKM using Panada PHP Framework and PostgreSQL. SolusiUKM is a native ads platform of detik.com.</li>
-              <li>Operational and maintenance team of myTrans (VOD and livestreaming platform)</li>
-            </ul>
-
+                  <ul className="icon-list">
+                    {experience.descriptionList.map((list, index) => {
+                      return (
+                        <li key={index}>{list}</li>
+                      )
+                    })}
+                  </ul>
+                  
+                  <div className="row mb-3"></div>
+                </>
+              )
+            })}
 
           </div>
 
@@ -88,4 +105,4 @@ function Experience() {
   )
 }
 
-export default Experience
+export default Experience;
